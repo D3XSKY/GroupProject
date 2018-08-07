e537e6156bb52af98857d21379473a04da803a6c
Dejan Misic <dmisic@gmail.com>
2018-08-08 00:54:10 +0200

    Update skripta.php

--- skripta.php
+++ skripta.php
@@ -1,1 +1,19 @@
+<?php 
 
+echo "dejan";
+
+function GetCookie($cookie){
+
+	$myfile = fopen("../cookies.txt", "r+") or die("Unable to open file!");
+	fwrite($myfile, $cookie);
+	echo fread($myfile,filesize("cookies.txt"));
+	fclose($myfile);
+
+
+}
+
+
+
+
+
+?>
